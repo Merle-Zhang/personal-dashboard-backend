aaeafa7d9c93ffaa5ec7c04453555abd8b0ea66c
Merle <merle.xzhang@gmail.com>
2023-05-01 14:34:12 +0000

    Initialize with `curl -G https://start.spring.io/starter.zip -d dependencies=data-rest,data-jpa,h2 -d type=maven-project -o demo.zip -d baseDir=personal-dashboard-backend -d applicationName=personal-dashboard-backend -d artifactId=personal-dashboard-backend -d description=The%20backend%20for%20personal%20dashboard%20project -d groupId=io.merle-zhang -d name=personal-dashboard-backend -d packageName=io.merle-zhang.personal-dashboard-backend`

---
+++ src/main/java/io/merlezhang/personaldashboardbackend/personal-dashboard-backend.java
@@ -0,0 +1,13 @@
+package io.merlezhang.personaldashboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class personal-dashboard-backend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(personal-dashboard-backend.class, args);
+	}
+
+}
---
+++ src/test/java/io/merlezhang/personaldashboardbackend/personal-dashboard-backendTests.java
@@ -0,0 +1,13 @@
+package io.merlezhang.personaldashboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class personal-dashboard-backendTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
