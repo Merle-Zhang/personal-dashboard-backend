6611ffb8ce763c4bb24dbbee504f35da90fcaa32
Merle <merle.xzhang@gmail.com>
2023-05-01 19:18:56 +0000

    Change from dash-case to PascalCase.

--- src/main/java/io/merlezhang/personaldashboardbackend/PersonalDashboardBackend.java
+++ src/main/java/io/merlezhang/personaldashboardbackend/PersonalDashboardBackend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class personal-dashboard-backend {
+public class PersonalDashboardBackend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(personal-dashboard-backend.class, args);
+		SpringApplication.run(PersonalDashboardBackend.class, args);
 	}
 
 }
--- src/test/java/io/merlezhang/personaldashboardbackend/PersonalDashboardBackendTests.java
+++ src/test/java/io/merlezhang/personaldashboardbackend/PersonalDashboardBackendTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class personal-dashboard-backendTests {
+class PersonalDashboardBackendTests {
 
 	@Test
 	void contextLoads() {
