9d276ed86a44fa59abd76adff31cbcdb03618627
Merle <merle.xzhang@gmail.com>
2023-05-02 00:19:10 +0000

    Change tabs to spaces.

--- src/main/java/io/merlezhang/personaldashboardbackend/PersonalDashboardBackend.java
+++ src/main/java/io/merlezhang/personaldashboardbackend/PersonalDashboardBackend.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonalDashboardBackend {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonalDashboardBackend.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PersonalDashboardBackend.class, args);
+  }
 
 }
